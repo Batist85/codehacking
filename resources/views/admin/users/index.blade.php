8c44706e4545830f3291a2356f0cf92be8bd6b53
Batist <tardiou@gmail.com>
2018-03-22 13:20:27 +0100

    Edit Users working except for security

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,6 +7,7 @@
     <thead class="thead-inverse">
       <tr>
         <th>#</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -20,7 +21,8 @@
         @foreach($users as $user)
           <tr>
             <td>{{$user->id}}</td>
-            <td>{{$user->name}}</td>
+            <td><img height="50" src="{{$user->photo ? $user->photo->file : '/image/placeholder.png'}}" alt=""></td>
+            <td><a href="{{route('admin.users.edit',$user->id)}}">{{$user->name}}</a></td>
             <td>{{$user->email}}</td>
             <td>{{$user->role->name}}</td>
             <td>{{$user->is_active == 1 ? 'active' : 'Not Active'}}</td>
