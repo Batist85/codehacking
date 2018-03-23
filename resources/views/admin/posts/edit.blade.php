4c80182937cbc695a124925c730a6402c5dcae49
Batist <tardiou@gmail.com>
2018-03-23 15:07:13 +0100

    edit post page is finished

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -2,6 +2,41 @@
 
 @section('content')
 
-    <h1>Edit Post</h1>
+    <h1>Edit Post by <span style="color: royalblue;">{{$post->user->name}}</span></h1>
+
+        <div class="col-sm-3">
+            <img src="{{$post->photo ? $post->photo->file : '/image/placeholder.png'}}" alt="" class="img-responsive img-rounded">
+        </div>
+
+    <div class="col-sm-9">
+
+    @include('includes.form_error')
+
+        {!! Form::model($post, ['method'=>'PATCH', 'action'=> ['AdminPostsController@update', $post->id], 'files'=>true]) !!}
+
+            <div class="form-group">
+                {!! Form::label('title', 'Title : ') !!}
+                {!! Form::text('title', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('category_id', 'Category : ') !!}
+                {!! Form::select('category_id', $categories, null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('photo_id', 'Photo : ') !!}
+                {!! Form::file('photo_id') !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('body', 'Content : ') !!}
+                {!! Form::textarea('body', null, ['class'=>'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::submit('Update Post', ['class'=>'btn btn-primary']) !!}
+            </div>
+
+        {!! Form::close() !!}
+
+    </div>
 
 @stop
+
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -24,8 +24,8 @@
                         <td>{{$post->user->name}}</td>
                         <td>{{$post->category ? $post->category->name : 'no categories'}}</td>
                         <td><img height="50" src="{{$post->photo ? $post->photo->file : '/image/placeholder.png'}}" alt=""></td>
-                        <td>{{$post->title}}</td>
-                        <td>{{$post->body}}</td>
+                        <td><a href="{{ route('admin.posts.edit', $post->id) }}">{{$post->title}}</a></td>
+                        <td>{{str_limit($post->body, 20)}}</td>
                         <td>{{$post->created_at->diffForhumans()}}</td>
                         <td>{{$post->updated_at->diffForhumans()}}</td>
                     </tr>
